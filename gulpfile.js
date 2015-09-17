ec4fdc8df12a6eda9739d46513f7dbca84a9233f
idleberg <jan@idleberg.com>
2015-09-17 15:04:00 +0200

    verbose linter output

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-// Requirements
-var gulp     = require('gulp');
-var recess   = require('gulp-recess');
+var gulp = require('gulp');
+var debug = require('gulp-debug');
 var jsonlint = require('gulp-json-lint');
 var coffeelint = require('gulp-coffeelint');
+
+// Tasks
+gulp.task('lint', ['coffeelint', 'jsonlint']);
 
 // Exclude node_modules
 var self = '!node_modules/**/*';
 
-// Tasks
-gulp.task('lint', ['coffeelint', 'jsonlint', 'less']);
-
 // Lint CSON files
 gulp.task('coffeelint', function () {
-    gulp.src(['./**/*.cson', self])
+    gulp.src([
+        './**/*.cson', './**/*.coffee', self])
+        .pipe(debug({title: 'csonlint:'}))
         .pipe(coffeelint())
         .pipe(coffeelint.reporter())
 });
@@ -20,13 +21,7 @@ gulp.task('coffeelint', function () {
 // Lint JSON files
 gulp.task('jsonlint', function() {
    gulp.src(['./**/*.json', self])
+      .pipe(debug({title: 'jsonlint:'}))
       .pipe(jsonlint())
       .pipe(jsonlint.report('verbose'));
-});
-
-// Lint LESS files
-gulp.task('less', function () {
-     gulp.src('index.less')
-        .pipe(recess({"noOverqualifying": false} ))
-        .pipe(recess.reporter());
 });
